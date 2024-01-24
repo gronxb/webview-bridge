28027b0c147d44ac8ebed191f42b50b433aae772
gronxb <gron1gh1@gmail.com>
2024-01-25 01:26:11 +0900

    fix: no generic eventName string type

--- packages/web/src/linkNativeEventListener.ts
+++ packages/web/src/linkNativeEventListener.ts
@@ -4,17 +4,21 @@ import { z } from "zod";
 import { EventBridge } from "./types";
 
 export const linkNativeEventListener = <
-  EventBridgeObject extends EventBridge<EventBridgeObject>,
+  EventBridgeObject extends EventBridge<EventBridgeObject> = {
+    __signature: "eventBridge";
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [key: string]: any;
+  },
 >() => {
   const emitter = createEvents();
 
   window.eventEmitter = emitter;
   return {
     on: <K extends keyof Omit<EventBridgeObject, "__signature">>(
-      event: K,
+      eventName: K,
       callback: (data: z.infer<EventBridgeObject[K]>) => void,
     ) => {
-      return emitter.on(event as string, callback);
+      return emitter.on(eventName as string, callback);
     },
   };
 };
