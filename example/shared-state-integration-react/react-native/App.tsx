9dd901b8a6152c5de25ca6842dfae20a162b5bce
gronxb <gron1gh1@gmail.com>
2024-01-30 23:57:29 +0900

    chore: examples

---
+++ example/shared-state-integration-react/react-native/App.tsx
@@ -0,0 +1,122 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from "react";
+import { Button, Text, SafeAreaView, TextInput, View } from "react-native";
+import {
+  createWebView,
+  type BridgeWebView,
+  bridge,
+  Bridge,
+  useBridge,
+} from "@webview-bridge/react-native";
+import InAppBrowser from "react-native-inappbrowser-reborn";
+
+interface AppBridgeState extends Bridge {
+  getMessage(): Promise<string>;
+  openInAppBrowser(url: string): Promise<void>;
+  count: number;
+  increase(): Promise<void>;
+  data: {
+    text: string;
+  };
+  setDataText(text: string): Promise<void>;
+}
+
+export const appBridge = bridge<AppBridgeState>(({ get, set }) => ({
+  async getMessage() {
+    return "I'm from native" as const;
+  },
+  async openInAppBrowser(url: string) {
+    if (await InAppBrowser.isAvailable()) {
+      await InAppBrowser.open(url);
+    }
+  },
+
+  data: {
+    text: "",
+  },
+  count: 0,
+  async increase() {
+    set({
+      count: get().count + 1,
+    });
+  },
+  async setDataText(text) {
+    set({
+      data: {
+        text,
+      },
+    });
+  },
+}));
+
+// It is exported via the package.json type field.
+export type AppBridge = typeof appBridge;
+
+export const { WebView, linkWebMethod } = createWebView({
+  bridge: appBridge,
+  debug: true,
+  fallback: (method) => {
+    console.warn(`Method '${method}' not found in native`);
+  },
+});
+
+function Count() {
+  // render when count changed
+  const count = useBridge(appBridge, (state) => state.count);
+
+  return <Text>Native Count: {count}</Text>;
+}
+
+function Input() {
+  const { data, setDataText } = useBridge(appBridge);
+
+  return (
+    <View>
+      <Text>Native Count: {data.text}</Text>
+      <TextInput
+        value={data.text}
+        // onChangeText={setDataText}
+        style={{ borderWidth: 1, width: "50%" }}
+      />
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  const webviewRef = React.useRef<BridgeWebView>(null);
+
+  const increase = useBridge(appBridge, (state) => state.increase);
+
+  return (
+    <SafeAreaView style={{ height: "100%" }}>
+      <WebView
+        ref={webviewRef}
+        source={{
+          uri: "http://localhost:5173",
+        }}
+        style={{ height: "50%", width: "100%" }}
+      />
+
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "50%",
+        }}
+      >
+        <Count />
+        <Button onPress={() => increase()} title="Increase From Native" />
+
+        <Input />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ example/shared-state-integration-react/react/src/App.tsx
@@ -0,0 +1,67 @@
+import "./App.css";
+import { linkBridge } from "@webview-bridge/web";
+import type { AppBridge } from "@webview-bridge-example-native-method/react-native";
+import { useBridge } from "./useBridge";
+
+const bridge = linkBridge<AppBridge>({
+  throwOnError: true,
+  onReady: () => {
+    console.log("nativeMethod is ready");
+  },
+});
+
+function Count() {
+  const count = useBridge(bridge.store, (state) => state.count);
+
+  return <p>Native Count: {count}</p>;
+}
+
+function DataText() {
+  const text = useBridge(bridge.store, (state) => state.data.text);
+
+  return (
+    <div>
+      <p>Native Data Text: {text}</p>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => bridge.setDataText(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function App() {
+  const increase = useBridge(bridge.store, (state) => state.increase);
+
+  return (
+    <div>
+      <div>
+        {`isWebViewBridgeAvailable: ${String(bridge.isWebViewBridgeAvailable)}`}
+      </div>
+
+      <button
+        onClick={() => {
+          if (bridge.isNativeMethodAvailable("openInAppBrowser") === true) {
+            bridge.openInAppBrowser("https://github.com/gronxb/webview-bridge");
+          }
+        }}
+      >
+        open InAppBrowser
+      </button>
+
+      <Count />
+      <button
+        onClick={() => {
+          increase(); // or bridge.increase()
+        }}
+      >
+        Increase from web
+      </button>
+
+      <DataText />
+    </div>
+  );
+}
+
+export default App;
