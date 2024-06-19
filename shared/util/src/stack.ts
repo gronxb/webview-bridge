2003f6c23f7a32589668f41bfaf5d0a6147a2c57
gronxb <gron1gh1@gmail.com>
2024-06-20 00:30:29 +0900

    fix: stack-based ref management

---
+++ shared/util/src/stack.ts
@@ -0,0 +1,27 @@
+export class Stack<T> {
+  private items: T[] = [];
+
+  push(element: T): void {
+    this.items.push(element);
+  }
+
+  pop(): T | undefined {
+    return this.items.pop();
+  }
+
+  top(): T | undefined {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  size(): number {
+    return this.items.length;
+  }
+
+  clear(): void {
+    this.items = [];
+  }
+}
