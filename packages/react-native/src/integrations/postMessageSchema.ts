ed7f7d48301d06a708335aea2f39cf4259442f05
gronxb <gron1gh1@gmail.com>
2024-06-25 23:09:24 +0900

    refactor: variable name

--- packages/react-native/src/integrations/postMessageSchema.ts
+++ packages/react-native/src/integrations/postMessageSchema.ts
@@ -3,61 +3,63 @@ import { ParserSchema, PostMessageSchemaObject } from "@webview-bridge/types";
 export const postMessageSchema = <T extends PostMessageSchemaObject>(
   schema: T,
 ): ParserSchema<T> => {
-  const parserSchema = Object.entries(schema).map(([key, value]) => {
-    // zod
-    if ("parse" in value && typeof value.parse === "function") {
-      return [
-        key,
-        {
-          parse: value.parse,
-          schema: value,
-        },
-      ];
-    }
-
-    // yup
-    else if (
-      "validateSync" in value &&
-      typeof value.validateSync === "function"
-    ) {
-      return [
-        key,
-        {
-          parse: (data: any) => {
-            return value.validateSync(data, { abortEarly: true });
+  const parserSchema = Object.entries(schema).map(
+    ([eventName, eventSchema]) => {
+      // zod
+      if ("parse" in eventSchema && typeof eventSchema.parse === "function") {
+        return [
+          eventName,
+          {
+            parse: eventSchema.parse,
+            schema: eventSchema,
           },
-          schema: value,
-        },
-      ];
-    }
+        ];
+      }
 
-    // superstruct
-    else if (
-      "assert" in value &&
-      typeof value.assert === "function" &&
-      "validate" in value &&
-      typeof value.validate === "function"
-    ) {
-      return [
-        key,
-        {
-          parse: (data: any) => {
-            void value.assert(data);
-            const [_, result] = value.validate(data);
-            return result;
+      // yup
+      else if (
+        "validateSync" in eventSchema &&
+        typeof eventSchema.validateSync === "function"
+      ) {
+        return [
+          eventName,
+          {
+            parse: (data: any) => {
+              return eventSchema.validateSync(data, { abortEarly: true });
+            },
+            schema: eventSchema,
           },
-          schema: value,
+        ];
+      }
+
+      // superstruct
+      else if (
+        "assert" in eventSchema &&
+        typeof eventSchema.assert === "function" &&
+        "validate" in eventSchema &&
+        typeof eventSchema.validate === "function"
+      ) {
+        return [
+          eventName,
+          {
+            parse: (data: any) => {
+              void eventSchema.assert(data);
+              const [_, result] = eventSchema.validate(data);
+              return result;
+            },
+            schema: eventSchema,
+          },
+        ];
+      }
+      return [
+        eventName,
+        {
+          parse: (data: any) => data,
+          schema: void 0,
         },
       ];
-    }
-    return [
-      key,
-      {
-        parse: (data: any) => data,
-        schema: void 0,
-      },
-    ];
-  });
+    },
+  );
 
   return Object.fromEntries(parserSchema);
 };
