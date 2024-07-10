30e5e37229574ce20cbe9cd2aeb2fdac7e6a1b1f
Sungyu Kang <gron1gh1@gmail.com>
2024-07-11 01:33:43 +0900

    chore: remove deprecated linkNativeMethod

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./error";
 export * from "./linkBridge";
-export * from "./linkNativeMethod";
 export * from "./registerWebMethod";
 export type * from "./types";
 export * from "@webview-bridge/types";
