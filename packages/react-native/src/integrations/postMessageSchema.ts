3b3bcb80f1894d27c64836b864cbd68901e31922
Sungyu Kang <gron1gh1@gmail.com>
2024-10-25 01:46:55 +0900

    fix: comment

--- packages/react-native/src/integrations/postMessageSchema.ts
+++ packages/react-native/src/integrations/postMessageSchema.ts
@@ -3,5 +3,6 @@ import type { PostMessageSchemaObject } from "@webview-bridge/types";
 export const postMessageSchema = <T extends PostMessageSchemaObject>(
   schema: T,
 ) => {
+  // Returning immediately to infer the correct type.
   return schema;
 };
