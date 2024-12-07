294b828cf9c2fc64fb3eb5b9012fc50452f79c6d
gronxb <gron1gh1@gmail.com>
2024-12-07 15:55:08 +0900

    fix: by id

--- packages/react-native/src/integrations/bridge.ts
+++ packages/react-native/src/integrations/bridge.ts
@@ -1,109 +1,115 @@
 import type {
-  Bridge,
-  BridgeStore,
-  OnlyJSON,
-  Primitive,
+	Bridge,
+	BridgeStore,
+	OnlyJSON,
+	Primitive,
 } from "@webview-bridge/types";
 import { equals, removeUndefinedKeys } from "@webview-bridge/utils";
-import WebView from "react-native-webview";
+import type WebView from "react-native-webview";
 
 export type StoreCallback<T> = ({
-  get,
-  set,
+	get,
+	set,
 }: {
-  get: () => T;
-  set: (newState: Partial<OnlyJSON<T>>) => void;
+	get: () => T;
+	set: (newState: Partial<OnlyJSON<T>>) => void;
 }) => T;
 
 export const bridge = <T extends Bridge>(
-  procedures: T | StoreCallback<T>,
+	procedures: T | StoreCallback<T>,
 ): BridgeStore<T> => {
-  const getState = () => state;
+	const getState = () => state;
 
-  const setState = (newState: Partial<OnlyJSON<T>>) => {
-    const _newState = {
-      ...state,
-      ...removeUndefinedKeys(newState),
-    };
+	const setState = (newState: Partial<OnlyJSON<T>>) => {
+		const _newState = {
+			...state,
+			...removeUndefinedKeys(newState),
+		};
 
-    if (equals(state, _newState)) {
-      return;
-    }
+		if (equals(state, _newState)) {
+			return;
+		}
 
-    const prevState = state;
-    state = _newState;
-    emitChange(state, prevState);
-  };
+		const prevState = state;
+		state = _newState;
+		emitChange(state, prevState);
+	};
 
-  let state: T =
-    typeof procedures === "function"
-      ? procedures({
-          get: getState,
-          set: setState,
-        })
-      : procedures;
+	let state: T =
+		typeof procedures === "function"
+			? procedures({
+					get: getState,
+					set: setState,
+				})
+			: procedures;
 
-  const listeners = new Set<(newState: T, prevState: T) => void>();
+	const listeners = new Set<(newState: T, prevState: T) => void>();
 
-  const emitChange = (newState: T, prevState: T) => {
-    for (const listener of listeners) {
-      listener(newState, prevState);
-    }
-  };
+	const emitChange = (newState: T, prevState: T) => {
+		for (const listener of listeners) {
+			listener(newState, prevState);
+		}
+	};
 
-  const subscribe = (listener: (newState: T, prevState: T) => void) => {
-    listeners.add(listener);
-    return () => listeners.delete(listener);
-  };
+	const subscribe = (listener: (newState: T, prevState: T) => void) => {
+		listeners.add(listener);
+		return () => listeners.delete(listener);
+	};
 
-  return {
-    getState,
-    setState,
-    subscribe,
-  } as BridgeStore<T>;
+	return {
+		getState,
+		setState,
+		subscribe,
+	} as BridgeStore<T>;
 };
 
 type HandleBridgeArgs<ArgType = unknown> = {
-  bridge: BridgeStore<Bridge>;
-  method: string;
-  args?: ArgType[];
-  webview: WebView;
-  eventId: string;
-  bridgeId: string;
+	bridge: BridgeStore<Bridge>;
+	method: string;
+	args?: ArgType[];
+	webview: WebView;
+	eventId: string;
+	bridgeId: string;
 };
 
 export const handleBridge = async ({
-  bridge,
-  method,
-  args,
-  webview,
-  eventId,
-  bridgeId,
+	bridge,
+	method,
+	args,
+	webview,
+	eventId,
+	bridgeId,
 }: HandleBridgeArgs) => {
-  const _bridge = bridge.getState();
+	const _bridge = bridge.getState();
 
-  const _method = _bridge[method];
-  const handleThrow = () => {
-    webview.injectJavaScript(SAFE_NATIVE_EMITTER_THROW(bridgeId, `${method}-${eventId}`));
-  };
-  if (!(method in _bridge)) {
-    handleThrow();
-    return;
-  }
-  if (typeof _method !== "function") {
-    return;
-  }
+	const _method = _bridge[method];
+	const handleThrow = () => {
+		webview.injectJavaScript(
+			SAFE_NATIVE_EMITTER_THROW_BY_BRIDGE_ID(bridgeId, `${method}-${eventId}`),
+		);
+	};
+	if (!(method in _bridge)) {
+		handleThrow();
+		return;
+	}
+	if (typeof _method !== "function") {
+		return;
+	}
 
-  try {
-    const response = await _method?.(...(args ?? []));
+	try {
+		const response = await _method?.(...(args ?? []));
 
-    webview.injectJavaScript(
-      SAFE_NATIVE_EMITTER_EMIT(bridgeId, `${method}-${eventId}`, response),
-    );
-  } catch (error) {
-    handleThrow();
-    console.error(error);
-  }
+		webview.injectJavaScript(
+			SAFE_NATIVE_EMITTER_EMIT_BY_BRIDGE_ID(
+				bridgeId,
+				`${method}-${eventId}`,
+				response,
+			),
+		);
+	} catch (error) {
+		handleThrow();
+		console.error(error);
+	}
 };
 
 export const INJECT_BRIDGE_METHODS = (bridgeNames: string[]) => `
@@ -111,15 +117,34 @@ export const INJECT_BRIDGE_METHODS = (bridgeNames: string[]) => `
 `;
 
 export const INJECT_BRIDGE_STATE = (
-  initialState: Record<string, Primitive>,
+	initialState: Record<string, Primitive>,
 ) => `
     window.__bridgeInitialState__ = ${JSON.stringify(initialState)};
 `;
 
-export const SAFE_NATIVE_EMITTER_EMIT = (bridgeId: string, eventName: string, data: unknown) => {
-  const dataString = JSON.stringify(data);
-  return `
-if (window.nativeEmitter['${bridgeId}']) {
+export const SAFE_NATIVE_EMITTER_EMIT = (eventName: string, data: unknown) => {
+	const dataString = JSON.stringify(data);
+	return `
+if (window.nativeEmitter && Object.keys(window.nativeEmitter).length > 0) {
+  for (const [_, emitter] of Object.entries(window.nativeEmitter)) {
+    emitter.emit('${eventName}', ${dataString});
+  }
+} else {
+  window.nativeBatchedEvents = window.nativeBatchedEvents || [];
+  window.nativeBatchedEvents.push(['${eventName}', ${dataString}]);
+}
+true;
+`;
+};
+
+export const SAFE_NATIVE_EMITTER_EMIT_BY_BRIDGE_ID = (
+	bridgeId: string,
+	eventName: string,
+	data: unknown,
+) => {
+	const dataString = JSON.stringify(data);
+	return `
+if (window.nativeEmitter && window.nativeEmitter['${bridgeId}']) {
   window.nativeEmitter['${bridgeId}'].emit('${eventName}', ${dataString});
 } else {
   window.nativeBatchedEvents = window.nativeBatchedEvents || [];
@@ -129,7 +154,10 @@ true;
 `;
 };
 
-export const SAFE_NATIVE_EMITTER_THROW = (bridgeId: string, eventName: string) => `
+export const SAFE_NATIVE_EMITTER_THROW_BY_BRIDGE_ID = (
+	bridgeId: string,
+	eventName: string,
+) => `
 if (window.nativeEmitter['${bridgeId}']) {
   window.nativeEmitter['${bridgeId}'].emit('${eventName}', {}, true);
 } else {
